53e53471043c485bc50eb05ba5600ca42fe4819e
MAHAMUDUL HASAN MITHUN <mhmmithun1@gmail.com>
2025-03-02 03:53:35 +0600

    Completed Desk page and many javascript addition

--- jsFIles/randomTheme.js
+++ jsFIles/randomTheme.js
@@ -1,4 +1,6 @@
 document.getElementById('randomTheme').addEventListener('click', function () {
     const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
     document.body.style.backgroundColor = randomColor;
+
+    
 });
